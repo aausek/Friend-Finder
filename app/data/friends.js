7505302b650f3fd9c6f6653c81937ebef6f82f59
Ana Ausek <anac_ausek@hotmail.com>
2017-04-28 21:16:30 -0400

    heroku deployment pending

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,15 @@ var friends = [
 	friendly = {
 		name: "Ana",
 		photo: "X",
-		scores: []
+		scores: ["5","5","5","5","5","5","5","5","5","5"]
+	},{
+		name: "Charlie",
+		photo: "Y",
+		scores: ["3","2","1","5","4","2","3","1","1","5"]
+	},{
+		name: "Spongebob",
+		photo: "Z",
+		scores: ["5","2","1","5","5","3","5","4","5","1"]
 	}
 ];
 
