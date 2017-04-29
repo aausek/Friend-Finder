b4e9084ca9532ba5b6dfc2d7bef38c80fef63473
Ana Ausek <anac_ausek@hotmail.com>
2017-04-28 22:07:11 -0400

    added input validation function to survey.html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 //"friends" array that holds dummy data but will be used to push new data into
 var friends = [
-	friendly = {
+	{
 		name: "Ana",
 		photo: "X",
 		scores: ["5","5","5","5","5","5","5","5","5","5"]
